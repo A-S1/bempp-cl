5558fe73249fae6d37f97537baf59c98682e651a
AS-1 <andrea.stanziale@gmail.com>
2025-03-07 15:53:38 +0100

    bug fixing geometric quantities

--- examples/maxwell/maxwell_screen.py
+++ examples/maxwell/maxwell_screen.py
@@ -78,7 +78,7 @@ grid1 = bempp.api.shapes.screen(corners1)
 grid2 = bempp.api.shapes.screen(corners2)
 grid3 = bempp.api.shapes.screen(corners3)
 
-grid4 = bempp.api.shapes.wire(wire_1, 0.1, 0.1)
+# grid4 = bempp.api.shapes.wire(wire_1, 0.1, 0.1)
 grid = bempp.api.grid.union([grid1, grid2, grid3])
 
 # -
