4c8ec42503e42a7c5b7456cd69b7c840080d3224
AS-1 <andrea.stanziale@gmail.com>
2025-04-11 14:20:05 +0200

    fix local accumulation

--- bempp/core/numba_kernels.py
+++ bempp/core/numba_kernels.py
@@ -2727,23 +2727,14 @@ def thinwire_efield_regular_assembler(
                             # we need the I_1 and I_4 components (Wilton) (the other ones are going to be handled in the singular kernel)
                             # another change of variable need to be applied (somewhere, maybe with the integration factor?)
                             # we get I_1 = int_(U_1) B(ksi_1,ksi_2)K(z,z+(rho+a)sinh u)R_max du, I_4 = int_(U_4) B(ksi_1,ksi_2)K(z,z+(rho+a)sinh u)R_max du
-                            local_result[
-                                trial_element_index, test_fun_index, trial_fun_index
-                            ] += tmp[
-                                trial_element_index * n_quad_points + quad_point_index
-                            ] * (
-                                -1j
-                                * wavenumber
-                                * test_basis_functions[
-                                    i, test_fun_index, :, test_point_index
-                                ] *
-                                    trial_basis_functions[
-                                        trial_element_index,
-                                        trial_fun_index,
-                                        :,
-                                        quad_point_index,
-                                    ]
-                                
+                            # Compute the dot product (or sum) over the vector components.
+                            dot_prod = _np.dot(
+                                test_basis_functions[i, test_fun_index, :, test_point_index],
+                                trial_basis_functions[trial_element_index, trial_fun_index, :, quad_point_index]
+                            )
+                            local_result[trial_element_index, test_fun_index, trial_fun_index] += (
+                                tmp[trial_element_index * n_quad_points + quad_point_index]
+                                * (-1j * wavenumber * dot_prod)
                             )
             print("Local Result: ", local_result)  
 
