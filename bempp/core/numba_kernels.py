7b2ddb590552a4dbe239745c623a518b75103bd7
AS-1 <andrea.stanziale@gmail.com>
2025-04-13 15:14:48 +0200

    fix dim helholtz single layer

--- bempp/core/numba_kernels.py
+++ bempp/core/numba_kernels.py
@@ -430,7 +430,7 @@ def helmholtz_single_layer_regular(
     else:
         for i in range(3):
             for j in range(npoints):
-                dist[j] += (trial_points[j] - test_point[i]) ** 2
+                dist[j] += (trial_points[i, j] - test_point[i]) ** 2
                 dist[j] += wire_radius ** 2
     for j in range(npoints):
         dist[j] = _np.sqrt(dist[j])
