55113217a2b1225a43599b442bd91f22d9a81190
AS-1 <andrea.stanziale@gmail.com>
2025-04-13 18:25:52 +0200

    Revert "reshaping global points"
    This reverts commit 65ba486c8cac65f9ce78f3efb161c67956b0322d.

--- bempp/core/numba_kernels.py
+++ bempp/core/numba_kernels.py
@@ -2691,7 +2691,6 @@ def thinwire_efield_regular_assembler(
         local_result = _np.zeros((n_trial_elements, nshape_test, nshape_trial), dtype=result_type)
         # Map the quadrature points on the test element to global coordinates.
         test_global_points = test_grid_data.local2global(test_element, quad_points)
-        test_global_points = test_global_points.reshape(1, n_quad_points)	
 
         print("Test Global Points: ", test_global_points)	
         # Local factors combine the integration measure from both test and trial elements.
