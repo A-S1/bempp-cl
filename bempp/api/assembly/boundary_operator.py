cff96d6b1ffa3431aae1dd9e95464fb31e0b81eb
AS-1 <andrea.stanziale@gmail.com>
2025-04-18 15:24:38 +0200

    Revert "option to assemble only regular part"
    This reverts commit 5d1666f1f5e0fdce89b8e48cf5233ee75aeb1ece.

--- bempp/api/assembly/boundary_operator.py
+++ bempp/api/assembly/boundary_operator.py
@@ -33,10 +33,10 @@ class BoundaryOperator(object):
         """Return the parameters associated with the operator."""
         return self._parameters
 
-    def weak_form(self, option = None):
+    def weak_form(self):
         """Return the weak form (assemble if necessary)."""
         if not self._cached:
-            self._cached = self._assemble(option)
+            self._cached = self._assemble()
 
         return self._cached
 
@@ -122,11 +122,11 @@ class BoundaryOperatorWithAssembler(BoundaryOperator):
         """Operator descriptor."""
         return self._operator_descriptor
 
-    def _assemble(self, option = None):
+    def _assemble(self):
         """Assemble the operator."""
         if self.transpose_:
-            return self.assembler.assemble(self.descriptor, option).transpose()
-        return self.assembler.assemble(self.descriptor, option)
+            return self.assembler.assemble(self.descriptor).transpose()
+        return self.assembler.assemble(self.descriptor)
 
     def _transpose(self, _range):
         return BoundaryOperatorWithAssembler(
