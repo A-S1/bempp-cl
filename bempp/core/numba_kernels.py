5bd256b164d0e8797102840eee0432b9ad00627e
AS-1 <andrea.stanziale@gmail.com>
2025-04-28 16:30:52 +0200

    test

--- bempp/core/numba_kernels.py
+++ bempp/core/numba_kernels.py
@@ -2840,7 +2840,7 @@ def thinwire_efield_regular_assembler(
                         integrand = ((test_basis_deriv[test_fun_index, quad_point_index] @ dLG_int[quad_point_index, trial_element_index, trial_fun_index]) +
                                      k2 * (test_basis_functions[i, test_fun_index, :, quad_point_index] @ LG_int[quad_point_index, trial_element_index, trial_fun_index]))
                         local_result[trial_element_index, test_fun_index, trial_fun_index] += integrand * (quad_weights[quad_point_index] * local_test_factor)
-                    print(f"test_fun_index: {test_fun_index}, quad_point_index: {quad_point_index}, integrand: {integrand}, local_result: {local_result[trial_element_index, test_fun_index, :]}")
+                    print(f"test_element: {i}, trial_element: {trial_element_index}, test_fun_index: {test_fun_index}, trial_fun_index: {trial_fun_index}, integrand: {integrand}, local_result: {local_result[trial_element_index, test_fun_index, :]}")
                     
 
         # --- Accumulate the Local Results into the Global Matrix ---
