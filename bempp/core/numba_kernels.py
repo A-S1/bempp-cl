664a15dbe3f29d9f997e811ad4530abd1b28f021
AS-1 <andrea.stanziale@gmail.com>
2025-06-16 13:12:56 +0200

    Revert "test"
    This reverts commit e3f70e89d33eede8d76f3752c4234248df573770.

--- bempp/core/numba_kernels.py
+++ bempp/core/numba_kernels.py
@@ -2790,7 +2790,7 @@ def thinwire_efield_regular_assembler(
         trial_element = trial_elements[trial_element_index]
         for trial_point_index in range(n_quad_points):
             factors[trial_element_index * n_quad_points + trial_point_index] = (
-                quad_weights[trial_point_index] ) * 1 / trial_edge_lengths[trial_element]
+                quad_weights[trial_point_index] ) * trial_edge_lengths[trial_element]
             #)
 
     # --- Main Assembly Loop over Test Elements (Parallelized) ---
@@ -2805,7 +2805,7 @@ def thinwire_efield_regular_assembler(
         test_global_points = test_grid_data.local2global(test_element, quad_points)
         
         # The integration factor for the test element (its segment length)
-        local_test_factor = 1 / test_edge_lengths[i]
+        local_test_factor = test_edge_lengths[i]
 
         # --- Compute the Inner Integral: G(z) = int_L g(z,z') phi(z') dz' for each test quadrature point ---
         
@@ -2989,7 +2989,7 @@ def  thinwire_efield_singular(
                         nshape_trial * nshape_test * index
                         + test_fun_index * nshape_trial
                         + trial_fun_index
-                    ] += inv4pi * local_result #* test_edge_lengths[index]
+                    ] += inv4pi * local_result * test_edge_lengths[index]
         
 
                 
