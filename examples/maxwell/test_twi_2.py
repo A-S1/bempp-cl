9b3b049db254b113c5abe2ef588fddf781ccdac3
AS-1 <andrea.stanziale@gmail.com>
2025-06-23 17:10:56 +0200

    test 5

--- examples/maxwell/test_twi_2.py
+++ examples/maxwell/test_twi_2.py
@@ -45,7 +45,7 @@ grid21 = bempp.api.import_grid("examples/maxwell/line_mesh_long_21.msh")
 grid31 = bempp.api.import_grid("examples/maxwell/line_mesh_long_31.msh")
 grid51 = bempp.api.import_grid("examples/maxwell/line_mesh_long_51.msh")
 
-grid_list = [grid7]#, grid11, grid15, grid21, grid31, grid51]
+grid_list = [grid7, grid11, grid15, grid21, grid31, grid51]
 max_list = []
 
 plt.figure(figsize=(10, 6))
@@ -116,7 +116,7 @@ plt.grid(True)
 plt.tight_layout()
 
     # Refine the grid for better resolution
-# plt.show()
+plt.show()
 
 nx = 300
 nz = 300
